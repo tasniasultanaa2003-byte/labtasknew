00078a33fbb66313ad32c074918bf6ba78266606
tasniasultanaa2003-byte <tasniasultanaa2003@gmail.com>
2025-12-05 22:15:03 +0600

    Create cf-53.cpp

---
+++ cf-53.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.H>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string x;
+        int y;
+        cin >> x;
+        y = (x[0]-'0')+(x[1]-'0');
+        cout << y << endl;
+    }
+    return 0;
+}
